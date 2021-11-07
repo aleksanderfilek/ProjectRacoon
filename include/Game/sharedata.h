f6d3e0d02955e9330e03bc0d1c10ad57b7c276b6
unknown <filekaleksander@gmail.com>
2021-11-07 19:57:06 +0100

    share data

--- include/Game/sharedata.h
+++ include/Game/sharedata.h
@@ -5,11 +5,21 @@
 
 typedef struct
 {
-  uint32_t capacity;
-  void** data;
-}GameShareData;
+  void* data;
+  char* name;
+  void (*delteFunc)(void*);
+} GameShareData;
 
-GameShareData* gameShareDataInit(uint32_t capacity);
+typedef struct
+{
+  uint32_t capacity;
+  GameShareData* datas;
+}GameSharedDataSystem;
+
+GameSharedDataSystem* gameShareDataInit(uint32_t capacity);
 void gameShareDataDestroy(void* ptr);
+void gameSharedDataAdd(GameSharedDataSystem* system, const char* name, void* variable, void (*delteFunc)(void*));
+void* gameSharedDataGet(GameSharedDataSystem* system, const char* name);
+void gameSharedDataRemove(GameSharedDataSystem* system, const char* name);
 
 #endif
--- source/sharedata.c
+++ source/sharedata.c
@@ -1,28 +1,109 @@
 #include"Game/sharedata.h"
 
 #include<stdlib.h>
+#include<string.h>
 
-GameShareData* gameShareDataInit(uint32_t capacity)
+static void remove(GameShareData* data);
+
+GameSharedDataSystem* gameShareDataInit(uint32_t capacity)
 {
-  GameShareData* shareData = (GameShareData*)malloc(sizeof(GameShareData));
+  GameSharedDataSystem* system = (GameSharedDataSystem*)malloc(sizeof(GameSharedDataSystem));
 
-  return shareData;
+  system->capacity = capacity;
+  system->datas = (GameShareData*)malloc(capacity * sizeof(GameShareData));
+  memset(system->datas, 0, capacity * sizeof(GameShareData));
+
+  return system;
 }
 
 void gameShareDataDestroy(void* ptr)
 {
-  GameShareData* shareData = (GameShareData*)ptr;
+  GameSharedDataSystem* system = (GameSharedDataSystem*)ptr;
 
-  for(int i = 0; i < shareData->capacity; i++)
+  for(int i = 0; i < system->capacity; i++)
   {
-    if(shareData->data[i] == NULL)
-    {
-      continue;
-    }
-
-    free(shareData->data[i]);
+    remove(&system->datas[i]);
   }
-  free(shareData->data);
+  free(system->datas);
 
   free(ptr);
+}
+
+void gameSharedDataAdd(GameSharedDataSystem* system, const char* name, 
+  void* variable, void (*delteFunc)(void*))
+{
+  int firstEmpty = -1;
+  for(int i = 0; i < system->capacity; i++)
+  {
+    GameShareData* data = &system->datas[i];
+    if(data->name != NULL)
+    {
+      if(strcmp(data->name, name) == 0)
+      {
+        data->data = variable;
+        data->delteFunc = delteFunc;
+      }
+    }
+    else if(firstEmpty == -1)
+    {
+      firstEmpty = i;
+    }
+  }
+
+  if(firstEmpty >= 0)
+  {
+    system->datas[firstEmpty].name = strdup(name);
+    system->datas[firstEmpty].data = variable;
+    system->datas[firstEmpty].delteFunc = delteFunc;
+  }
+
+  int index = system->capacity;
+  system->capacity++;
+  system->datas = (GameShareData*)realloc(system->datas, system->capacity *sizeof(GameShareData));
+  system->datas[firstEmpty].name = strdup(name);
+  system->datas[firstEmpty].data = variable;
+  system->datas[firstEmpty].delteFunc = delteFunc;  
+}
+
+void* gameSharedDataGet(GameSharedDataSystem* system, const char* name)
+{
+  for(int i = 0; i < system->capacity; i++)
+  {
+    GameShareData* data = &system->datas[i];
+    if(data != NULL && strcmp(data->name, name) == 0)
+    {
+      return data->data;
+    }
+  }
+
+  return NULL;
+}
+
+void gameSharedDataRemove(GameSharedDataSystem* system, const char* name)
+{
+  for(int i = 0; i < system->capacity; i++)
+  {
+    GameShareData* data = &system->datas[i];
+    if(data != NULL && strcmp(data->name, name) == 0)
+    {
+      remove(data);
+      return;
+    }
+  }
+}
+
+static void remove(GameShareData* data)
+{
+  if(data->data == NULL)
+  {
+    return;
+  }
+
+  if(data->delteFunc == NULL)
+  {
+    free(data->data);
+    return;
+  }
+
+  data->delteFunc(data->data);
 }
